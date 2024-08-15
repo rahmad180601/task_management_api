2f63c23f2ddf9f53fc0b6d05cc0526c47a472c53
rahmadnasirin <59747382+rahmad180601@users.noreply.github.com>
2024-08-15 16:52:00 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
-// Route::post('/logout', [UserController::class, 'logout']);
 
 Route::middleware(['auth:api'])->group(function () {
     Route::get('/user', [UserController::class, 'index']);
